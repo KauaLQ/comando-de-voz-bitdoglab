61bac76d0d35857dbb44510e36e1666b1a933d6e
KauaLQ <limakaua610@gmail.com>
2025-05-18 21:38:56 -0300

    adicionando timer que controlará os comandos

--- microphone_adc.c
+++ microphone_adc.c
@@ -5,17 +5,42 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/i2c.h"
 #include "hardware/adc.h"
 #include "hardware/uart.h"
 #include "pico/binary_info.h"
+#include "functions/ssd1306_i2c.h"
 
 #define ADC_NUM 2
 #define ADC_PIN (26 + ADC_NUM)
 #define ADC_VREF 3.3
 #define ADC_RANGE (1 << 12)
 #define ADC_CONVERT (ADC_VREF / (ADC_RANGE - 1))
+
+#define LED_PIN 13     // Pino onde o LED está conectado
+
+volatile char action[200];
+
+// Callback para verificar o valor da variável
+void repeating_timer_callback(struct repeating_timer *t) {
+    // Verifica se as palavras-chave estão presentes
+    if (strstr(action, "ascender") != NULL && strstr(action, "led") != NULL) {
+        gpio_put(LED_PIN, 1); // Liga o LED
+        strcpy(action, ""); // Limpa a string action corretamente
+    }
+    else if (strstr(action, "apagar") != NULL && strstr(action, "led") != NULL) {
+        gpio_put(LED_PIN, 0); // Liga o LED
+        strcpy(action, ""); // Limpa a string action corretamente
+    }
+    else if (strstr(action, "tocar") != NULL && strstr(action, "alarme") != NULL) {
+        //tocarAlarme(); // Referente ao buzzer
+        strcpy(action, ""); // Limpa a string action corretamente
+    }
+    
+}
 
 int main() {
     stdio_init_all();
@@ -31,7 +56,42 @@ int main() {
     adc_gpio_init( ADC_PIN);
     adc_select_input( ADC_NUM);
 
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0); // Desliga o LED inicialmente
+
     uint adc_raw;
+
+    // Configura o temporizador repetitivo para verificar o estado do botão a cada 100 ms.
+    struct repeating_timer timer;
+    add_repeating_timer_ms(100, repeating_timer_callback, NULL, &timer);
+
+    //configuração do display ssd1306
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    SSD1306_init();
+
+    // Área de renderização do display
+    struct render_area frame_area = {
+        start_col: 0,
+        end_col : SSD1306_WIDTH - 1,
+        start_page : 0,
+        end_page : SSD1306_NUM_PAGES - 1
+        };
+
+    calc_render_area_buflen(&frame_area);
+
+    // Buffer para o display
+    uint8_t buf[SSD1306_BUF_LEN];
+    memset(buf, 0, SSD1306_BUF_LEN);
+
+    WriteString(buf, 35, 32, "OUVINDO");
+
+    render(buf, &frame_area);
+
     while (1) {
         adc_raw = adc_read(); // tensão bruta do ADC
         printf("%.2f\n", adc_raw * ADC_CONVERT);
